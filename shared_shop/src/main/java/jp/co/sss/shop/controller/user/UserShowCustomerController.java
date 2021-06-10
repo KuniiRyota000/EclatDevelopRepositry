2eb7cd8a1837c04b22475a8af16e1e0ea6994ae3
岩田智佳 <t.iwatabeforward15@gmail.com>
2021-06-10 14:56:03 +0900

    2つの<html>ファイルともに、ボタンが押された際のth:action記述
    <UserShowCustomerController>
    RequestMapping内のpath,return
    記述

--- shared_shop/src/main/java/jp/co/sss/shop/controller/user/UserShowCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/user/UserShowCustomerController.java
@@ -4,20 +4,16 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import jp.co.sss.shop.bean.UserBean;
 import jp.co.sss.shop.entity.User;
 import jp.co.sss.shop.form.UserForm;
 import jp.co.sss.shop.repository.UserRepository;
-import jp.co.sss.shop.util.Constant;
 
 /**
  * 会員管理 表示機能(一般ユーザー)のコントローラクラス
@@ -31,28 +27,6 @@ public class UserShowCustomerController {
 	 */
 	@Autowired
 	UserRepository userRepository;
-
-	/**
-	 * 会員一覧表示処理
-	 *
-	 * @param model    Viewとの値受渡し
-	 * @param form     会員情報フォーム
-	 * @param pageable ページング情報
-	 * @return "" 会員情報一覧表示画面へ
-	 */
-	@RequestMapping(path = "", method = RequestMethod.GET)
-	public String showUserList(Model model, @ModelAttribute UserForm form, Pageable pageable) {
-
-		// 会員情報リストを取得
-		Page<User> userList = userRepository.findByDeleteFlagOrderByInsertDateDesc(Constant.NOT_DELETED, pageable);
-
-		// 会員情報をViewに渡す
-		model.addAttribute("pages", userList);
-		model.addAttribute("users", userList.getContent());
-		model.addAttribute("url", "");
-
-		return "";
-	}
 
 	/**
 	 * 会員詳細表示処理
