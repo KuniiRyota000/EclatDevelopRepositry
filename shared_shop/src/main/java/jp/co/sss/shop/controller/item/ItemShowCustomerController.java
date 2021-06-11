5cd6d3a900c84434eeccde9501eece688b549491
岩田智佳 <t.iwatabeforward15@gmail.com>
2021-06-11 16:07:45 +0900

    カテゴリ検索のコントローラを追記

--- shared_shop/src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
@@ -1,11 +1,7 @@
 package jp.co.sss.shop.controller.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import jp.co.sss.shop.repository.ItemRepository;
 
@@ -29,35 +25,57 @@ public class ItemShowCustomerController {
 	 * @param pageable ページング情報
 	 * @return "/" トップ画面へ
 	 */
-	@RequestMapping(path = "/")
-	public String index(Model model, Pageable pageable) {
+	//	@RequestMapping(path = "/item/list/category", method = RequestMethod.GET)
+	//			public String itemListCategory1(String categoryId, Model mode, Pageable pageable) {
+	//				Page<Item> pageList = itemRepository.
+	//				List<Item> ItemList = pageList.getContent();
+	//				model.addAttribute("pages", pageList);
+	//				model.addAttribute("items", ItemList);
+	//				return "";
+	//			}
+	//
+	//	@RequestMapping("/item/list/category/1")
+	//	public String itemListCategory(Integer categoryId, Model model) {
+	//		Category category = new Category();
+	//		category.setId(categoryId);
+	//		List<Item> items = itemRepository.findById(category);
+	//		// エンティティ内の検索結果をJavaBeansにコピー
+	//		List<ItemBean> itemBeanList = BeanCopy.copyEntityToItemBean(items);
+	//		model.addAttribute("category", items);
+	//		model.addAttribute("items", itemBeanList);
+	//		return "item/item_search_result/item_list_1";
+	//	}
 
-		return "index";
-	}
+	//	@RequestMapping(path = "/item/list/category/{sortType}", method = RequestMethod.GET)
+	//	public String itemListCategory(Integer categoryId, Model model) {
+	//		Category category = new Category();
+	//		category.setId(categoryId);
+	//		List<Item> item = itemRepository.findById(category);
+	//		// エンティティ内の検索結果をJavaBeansにコピー
+	//		List<ItemBean> itemBeanList = BeanCopy.copyEntityToItemBean(item);
+	//		model.addAttribute("item", itemBeanList);
+	//		return "item/item_search_result/item_list_1";
+	//	}
+	//}
 
-	@RequestMapping(path = "/item/list/category/1", method = RequestMethod.GET)
-	public String itemListCategory1(String categoryId) {
-		return "item_search_result";
-	}
-
-	@RequestMapping(path = "/item/list/category/{sortType}", method = RequestMethod.GET)
-	public String itemListCategory(String categoryId) {
-		return "item_search_result";
-	}
-
-	@RequestMapping(path = "/item/list/price", method = RequestMethod.GET)
-	public String itemListPrice(Integer minPrice, Integer maxPrice) {
-		return "item_search_result";
-	}
-
-	@RequestMapping(path = "/item/list/price/{sortType}", method = RequestMethod.GET)
-	public String itemListPriceSortType(Integer minPrice, Integer maxPrice) {
-		return "item_search_result";
-	}
-
-	@RequestMapping(path = "/item/detail")
-	public String itemDetail() {
-		return "item_detail";
-	}
-
+	//public String itemListCategory(String categoryId) {
+	//	return "/item/item_search_result/item_search_result";
+	//}
+	//
+	//	@RequestMapping(path = "/item/list/price", method = RequestMethod.GET)
+	//	public String itemListPrice(Integer minPrice, Integer maxPrice, Integer price) {
+	//		return "/item/item_search_result/item_search_result";
+	//	}
+	//
+	//	@RequestMapping(path = "/item/list/price/{sortType}", method = RequestMethod.GET)
+	//	public String itemListPriceSortType(Integer minPrice, Integer maxPrice, Integer price) {
+	//		return "/item/item_search_result/item_search_result";
+	//	}
+	//
+	//	@RequestMapping(path = "/item/detail")
+	//	public String itemDetail() {
+	//		return "/item/item_search_result/item_search_result";
+	//	}
+	//
+	//}
 }
