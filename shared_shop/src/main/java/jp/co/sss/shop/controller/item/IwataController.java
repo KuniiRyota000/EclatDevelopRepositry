c0567785b62e11050577b1e55e1554ed579aa147
岩田智佳 <t.iwatabeforward15@gmail.com>
2021-06-14 16:10:49 +0900

    カテゴリ検索、価格帯のコントローラー追記：会員詳細html,コントローラー追記

--- shared_shop/src/main/java/jp/co/sss/shop/controller/item/IwataController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/item/IwataController.java
@@ -1,12 +1,21 @@
 package jp.co.sss.shop.controller.item;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.shop.bean.ItemBean;
+import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.repository.ItemRepository;
+import jp.co.sss.shop.util.BeanCopy;
 
 /**
  * 商品管理 一覧表示機能(一般会員用)のコントローラクラス
@@ -14,7 +23,7 @@ import jp.co.sss.shop.repository.ItemRepository;
  * @author SystemShared
  */
 @Controller
-public class ItemShowCustomerController {
+public class IwataController {
 	/**
 	 * 商品情報
 	 */
@@ -28,11 +37,11 @@ public class ItemShowCustomerController {
 	 * @param pageable ページング情報
 	 * @return "/" トップ画面へ
 	 */
-	@RequestMapping(path = "/")
-	public String index(Model model, Pageable pageable, Integer salesFigures) {
-		model.addAttribute("items1", itemRepository.findBySalesFiguresOrderBySalesFiguresAsc(salesFigures, pageable));
-		return "index";
-	}
+	//	@RequestMapping(path = "/")
+	//	public String index(Model model, Pageable pageable, Integer salesFigures) {
+	//		model.addAttribute("items1", itemRepository.findBySalesFiguresOrderBySalesFiguresAsc(salesFigures, pageable));
+	//		return "index";
+	//	}
 
 	/**新着一覧表示*/
 	@RequestMapping(path = "/item/list/{sortType}")
@@ -48,14 +57,15 @@ public class ItemShowCustomerController {
 	 * @param pageable ページング情報
 	 * @return "/" トップ画面へ
 	 */
-	//	@RequestMapping(path = "/item/list/category", method = RequestMethod.GET)
-	//			public String itemListCategory1(String categoryId, Model mode, Pageable pageable) {
-	//				Page<Item> pageList = itemRepository.
-	//				List<Item> ItemList = pageList.getContent();
-	//				model.addAttribute("pages", pageList);
-	//				model.addAttribute("items", ItemList);
-	//				return "";
-	//			}
+	@RequestMapping(path = "/item/list/category/1", method = RequestMethod.GET)
+	public String itemListCategory1(Integer categoryId, Model model, Pageable pageable) {
+		Page<Item> itemList = itemRepository.findByCategoryIdOrderByInsertDateDesc(categoryId, pageable);
+		// エンティティ内の検索結果をJavaBeansにコピー
+		List<ItemBean> itemBeanList = BeanCopy.copyEntityToItemBean(itemList.getContent());
+		model.addAttribute("pages", itemList);
+		model.addAttribute("items", itemBeanList);
+		return "item/item_search_result/item_list";
+	}
 	//
 	//	@RequestMapping("/item/list/category/1")
 	//	public String itemListCategory(Integer categoryId, Model model) {
@@ -85,9 +95,12 @@ public class ItemShowCustomerController {
 	//	return "/item/item_search_result/item_search_result";
 	//}
 	//
-	//	@RequestMapping(path = "/item/list/price", method = RequestMethod.GET)
-	//	public String itemListPrice(Integer minPrice, Integer maxPrice, Integer price) {
-	//		return "/item/item_search_result/item_search_result";
+	@RequestMapping(path = "/item/list/price", method = RequestMethod.GET)
+	public String itemListPrice(Integer minPrice, Integer maxPrice, Model model, Pageable pageable) {
+		model.addAttribute("item", itemRepository.findByPriceBetween(minPrice, maxPrice, pageable));
+		return "/item/item_search_result/item_search_result";
+	}
+
 	//	}
 	//
 	//	@RequestMapping(path = "/item/list/price/{sortType}", method = RequestMethod.GET)
@@ -101,4 +114,23 @@ public class ItemShowCustomerController {
 	//	}
 	//
 	//}
+	@RequestMapping(path = "/item/detail/{id}")
+	public String showItem(@PathVariable int id, Model model) {
+
+		// 商品IDに該当する商品情報を取得
+		Item item = itemRepository.findById(id).orElse(null);
+
+		ItemBean itemBean = new ItemBean();
+
+		// Itemエンティティの各フィールドの値をItemBeanにコピー
+		BeanUtils.copyProperties(item, itemBean);
+
+		// 商品情報にカテゴリ名を設定
+		itemBean.setCategoryName(item.getCategory().getName());
+
+		// 商品情報をViewへ渡す
+		model.addAttribute("item", itemBean);
+
+		return "item/detail/item_detail";
+	}
 }
